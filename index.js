0635de3b8c64c19ef153d2caa88ac843b1fe8d84
George Sandoval <gssandoval09@gmail.com>
2021-10-23 00:27:29 -0500

    Worked on index.js file, creating message prompts

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs")
+const markdown = require("./db")
+const path = require("path")
+
+function promptUser(){
+    return inquirer.prompt([
+
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "ask",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"],
+        },
+
+        {
+            type: "input",
+            message: "What is the name of the department?",
+            name: "department"
+        },
+
+        {
+            type: "input",
+            message: "What would you like to do?",
+            name: "",
+        },
+
+        {
+            type: "input",
+            message: "",
+            name: "",
+        },
+
+        {
+            type: "input",
+            message: "",
+            name:"",
+        },
+    ])
+}
+
+    
+
+
